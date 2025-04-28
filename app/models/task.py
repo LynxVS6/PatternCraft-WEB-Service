3b2a1d301fb890008139895b20fd08e49a5a6063
LynxVS <Viktoriariseva@yandex.com>
2025-04-29 00:41:33 +0500

    refactor: setup structure

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,4 @@ from app.extensions import db
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    done = db.Column(db.Boolean, default=False)
-    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    pass
