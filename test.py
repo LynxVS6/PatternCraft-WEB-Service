1ee15a1c5f4960558b24f5e1a5c117550b9d2bc4
LynxVS <Viktoriariseva@yandex.com>
2025-05-12 20:30:33 +0500

    feat: add Docker Compose configuration for MySQL service

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask_mail import Message
-from app.extensions import mail
-from app import create_app
-
-app = create_app()
-with app.app_context():
-    msg = Message("Тест Gmail SMTP", recipients=["lynxvs66@gmail.com"])
-    msg.body = "Если это письмо пришло — соединение с Gmail SMTP настроено верно!"
-    mail.send(msg)
