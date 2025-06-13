89732d6e7257ebecf8b2c52acfe8cb72b45e4ce8
Lenar Gatin <lgatin1711@gmail.com>
2025-06-13 16:38:44 +0500

    chore: update services __init__.py file

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,15 +1,25 @@
+from .auth_service import AuthService
+from .base_service import BaseService
 from .bookmark_service import BookmarkService
 from .comment_service import CommentService
 from .description_service import DescriptionService
-from .vote_service import ArrowVoteService, EmojiVoteService, LikeVoteService
 from .email_service import send_confirmation_email
+from .problem_service import ProblemService
+from .solution_service import SolutionService
+from .user_service import UserService
+from .vote_service import ArrowVoteService, EmojiVoteService, LikeVoteService
 
 __all__ = [
+    "AuthService",
+    "BaseService",
     "BookmarkService",
     "CommentService",
     "DescriptionService",
+    "ProblemService",
+    "SolutionService",
+    "UserService",
     "ArrowVoteService",
     "EmojiVoteService",
     "LikeVoteService",
-    'send_confirmation_email'
+    "send_confirmation_email",
 ]
